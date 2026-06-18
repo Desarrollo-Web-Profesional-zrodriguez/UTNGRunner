4e566738610c1317d58f12d5736660428f10dad3
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-06-17 19:12:22 -0600

    fix: use wear compose material theme instead of material3

--- app/src/main/java/mx/utng/utngrunner/presentation/theme/Theme.kt
+++ app/src/main/java/mx/utng/utngrunner/presentation/theme/Theme.kt
@@ -1,16 +1,12 @@
 package mx.utng.utngrunner.presentation.theme
 
 import androidx.compose.runtime.Composable
-import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.material.MaterialTheme
 
 @Composable
 fun UTNGRunnerTheme(
     content: @Composable () -> Unit
 ) {
-    /**
-     * Empty theme to customize for your app.
-     * See: https://developer.android.com/jetpack/compose/designsystems/custom
-     */
     MaterialTheme(
         content = content
     )
